9de2dd1945fa420509853202618fdccea336a829
Prateek <prateeksagar806@gmail.com>
2024-12-24 11:26:54 +0530

    Initialization of project

---
+++ src/main/java/com/seatacrental/seatacrental/SeatacRentalApplication.java
@@ -0,0 +1,13 @@
+package com.seatacrental.seatacrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatacRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatacRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seatacrental/seatacrental/SeatacRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatacrental.seatacrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatacRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
